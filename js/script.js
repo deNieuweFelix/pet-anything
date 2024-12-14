bac0da068b926ea66f67eaae813a3515b3496096
Felix Nieuwenhuijsen <6042715@mborijnland.nl>
2024-12-14 22:35:05 +0100

    yeah, we got those commands

--- js/script.js
+++ js/script.js
@@ -7,9 +7,12 @@ const settings = document.getElementById("settings");
 const customizeWindow = document.getElementById("customize");
 
 const bgColorInput = document.getElementById("bgColor");
+const instantLoadCheck = document.getElementById("instantLoadCheck");
 
 const terminal = document.getElementById("terminal");
 const terminalConsole = document.getElementById("terminalConsole");
+
+const allMessages = [];
 
 terminalConsole.addEventListener("keydown", function(e){
     if(e.code === "Enter"){
@@ -18,10 +21,73 @@ terminalConsole.addEventListener("keydown", function(e){
             case '?help':
                 fTerminal.write(
                     `Currently available commands:<br>
-                    - ?help: shows all commands`);
+                    - ?help: shows all commands <br>
+                    - !clr: clears the console`);
+                break;
+            case '!clr':
+                allMessages.forEach(msg => {
+                    msg.remove();
+                });
+                allMessages = [];
                 break;
             default:
                 fTerminal.write('> ' + msg);
+                //general commands
+
+                //commands that will execute something start with a !
+                if(msg.includes("!")){
+                    if(msg.includes("instaLoad")){
+                        if(msg.includes("true")){
+                            fTerminal.write("instantLoad: on", 'green');
+                            CustomSettings.instantLoad = true;
+                        }else if(msg.includes("false")){
+                            fTerminal.write("instantLoad: off", 'red');
+                            CustomSettings.instantLoad = false;
+                        }else{
+                            fTerminal.write("incorrect syntax: !instantLoad true / false", 'orange');
+                        }
+                    }else if(msg.includes("bgCol")){
+                        if(msg.includes("reset")){
+                            fTerminal.write("reset bgCol", 'green');
+                            document.body.style.backgroundColor = '#000011';
+                            document.body.style.color = '#FFFFFF';
+                        }else if(msg.includes("white")){
+                            fTerminal.write("set bgCol to white", 'green');
+                            document.body.style.backgroundColor = '#FFFFFF';
+                        }else if(msg.includes("light")){
+                            fTerminal.write("light mode: on", 'green');
+                            document.body.style.backgroundColor = `#FFFFFF`;
+                            document.body.style.color = '#000000';
+                        }else if(msg.includes("oled")){
+                            fTerminal.write("oled mode: on", 'green');
+                            document.body.style.backgroundColor = '#000000';
+                            document.body.style.color = '#FFFFFF';
+                        }else if(msg.includes("hell")){
+                            fTerminal.write("Why?", 'purple');
+                            document.body.style.backgroundColor = 'red';
+                            document.body.style.color = 'green';
+                        }
+                    }else if(msg.includes("title")){
+                        if(msg.includes("!set")){
+                            CustomSettings.customTitle = prompt("set title:");
+                            document.title = CustomSettings.customTitle;
+                            fTerminal.write(`title set to: ${CustomSettings.customTitle}`, 'green');
+                        }else if(msg.includes("reset")){
+                            CustomSettings.customTitle = '';
+                            document.title = "Pet anything!";
+                            fTerminal.write('resetted title', 'green');
+                        }
+                    }else if(msg.includes("!!reload")){
+                        location.reload();
+                    }
+                }else if(msg.includes("?")){
+                    //commands that will retrieve a value
+                    if(msg.includes("instaLoad")){
+                        fTerminal.write(`instaload: ${CustomSettings.instantLoad}`, 'blue');
+                    }else if(msg.includes("bgCol")){
+                        fTerminal.write(`backgroundCol: ${CustomSettings.bgColor}`, 'blue');
+                    }
+                }
             break;
         }
     } 
@@ -36,7 +102,8 @@ let customizeWindowStatus = {
 //you can change these settings in the source code to change them on load, lol
 let CustomSettings = {
     'bgColor': '#000011',
-    'instantLoad': false
+    'instantLoad': false,
+    'customTitle': ''
 };
 
 let imageDone = false;
@@ -45,23 +112,24 @@ let imageDone = false;
 function onLoad(){
     customizeWindow.remove();
 
-    setTimeout(function(){
-        if(CustomSettings.instantLoad == true){
-            // imageInput.addEventListener('change', displayImage(this.files[0]));
-            let checkInterval = setInterval(function(){
-                if(imageDone == true){
-                    clearInterval(checkInterval);
-                    setTimeout(function(){
-                        console.clear();
-                        setTimeout(() =>{
-                            console.log("I did NOT just remove all the errors... \nwhy would i?");
-                        })
-                    }, 150);
-                }
-                displayImage(imageInput.files[0]);
-            }, 1000);
-        }
-    }, 150);
+    setTimeout(autoLoad, 150);
+}
+
+function autoLoad(){
+    if(CustomSettings.instantLoad == true){
+        let checkInterval = setInterval(function(){
+            if(imageDone == true){
+                clearInterval(checkInterval);
+                setTimeout(function(){
+                    console.clear();
+                    setTimeout(() =>{
+                        console.log("I did NOT just remove all the errors... \nwhy would i?");
+                    })
+                }, 150);
+            }
+            displayImage(imageInput.files[0]);
+        }, 1000);
+    }
 }
 
 
@@ -120,15 +188,29 @@ function changeSetting(setting){
 
             fTerminal.write(`bg changed to: ${CustomSettings.bgColor}`);
             break;
+        case 1:
+            if(instantLoadCheck.checked){
+                fTerminal.write("instantLoad: on", 'green');
+                CustomSettings.instantLoad = true;
+            }else{
+                fTerminal.write("instantLoad: off", 'red');
+                CustomSettings.instantLoad = false;
+            }
         default:
             break;
     }
 }
 
 const fTerminal = {
-    write: function(msg){
+    write: function(msg, clr){
         var msgP = document.createElement("p");
         terminal.appendChild(msgP);
+        msgP.style.color = clr;
         msgP.innerHTML = msg;
+        allMessages.push(msgP);
     }
 }
+
+var mainLongLoop = setInterval(function(){
+    autoLoad();
+}, 2000);
